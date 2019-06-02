fc4b3e97e92bd7163636b38911eed7cc44e33295
alex-sir <alexcarbajal710@gmail.com>
2019-06-02 00:04:53 -0700

    Finish product showcase #1 & add shipping section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 const bottles = [...document.querySelectorAll('.bottle')];
 const bottleNames = document.querySelectorAll('.bottle-names>span');
+const phoneMediaQuery = window.matchMedia('(min-width: 640px)');
 
-bottles.forEach(bottle => {
-  bottle.addEventListener('mouseover', () => {
-    bottleNames[bottles.indexOf(bottle)].setAttribute('style', 'opacity: 1;');
-  });
-  bottle.addEventListener('mouseout', () => {
-    bottleNames[bottles.indexOf(bottle)].setAttribute('style', 'opacity: 0;');
-  });
-});
+function bottleNameListener(e) {
+    if (e.matches) {
+        bottles.forEach(bottle => {
+            bottle.addEventListener('mouseover', () => {
+                bottleNames[bottles.indexOf(bottle)].setAttribute('style', 'opacity: 1;');
+            });
+            bottle.addEventListener('mouseout', () => {
+                bottleNames[bottles.indexOf(bottle)].setAttribute('style', 'opacity: 0;');
+            });
+        });
+    } else {
+        bottleNames[0].style.opacity = '1';
+    }
+}
+
+phoneMediaQuery.addListener(bottleNameListener);
