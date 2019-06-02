0132fe2f994f03aabd2477f90250fccf60d87648
alex-sir <alexcarbajal710@gmail.com>
2019-06-02 16:08:30 -0700

    Finish product showcase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 const bottles = [...document.querySelectorAll('.bottle')];
-const bottleNames = document.querySelectorAll('.bottle-names>span');
+const bottleNames = document.querySelectorAll('.bottle-name');
 const phoneMediaQuery = window.matchMedia('(min-width: 640px)');
 
 function bottleNameListener(e) {
     if (e.matches) {
+        bottleNames[0].style.opacity = '0';
+        bottleNames[3].style.opacity = '0';
         bottles.forEach(bottle => {
             bottle.addEventListener('mouseover', () => {
                 bottleNames[bottles.indexOf(bottle)].setAttribute('style', 'opacity: 1;');
@@ -14,7 +16,9 @@ function bottleNameListener(e) {
         });
     } else {
         bottleNames[0].style.opacity = '1';
+        bottleNames[3].style.opacity = '1';
     }
 }
 
 phoneMediaQuery.addListener(bottleNameListener);
+bottleNameListener(phoneMediaQuery);
